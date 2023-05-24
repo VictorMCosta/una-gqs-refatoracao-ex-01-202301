0921ad682c5e942850a197cb4a6ad138e6145c04
Víctor Mendes Amaral Costa <victormendes0203@gmail.com>
2023-05-24 20:03:02 -0300

    fix: correção de validação de classe

--- Programa.java
+++ Programa.java
@@ -10,15 +10,15 @@ class Programa {
 
     System.out.println("\tRelatório Auxiliar de Controle de Infecções");
 
-    pessoa.coletaNome(sc);
-    pessoa.coletaIdade(sc);
+    pessoa.coletaNome(pessoa, scanner);
+    pessoa.coletaIdade(pessoa, scanner);
 
-    erroTentativas = mensagem.realizaPerguntaVacinaEmDia(pesssoa);
+    erroTentativas = mensagem.realizaPerguntaVacinaEmDia(pessoa, scanner);
 
-    validacao.validacaoDasProximasPerguntas(pessoas);
+    validacao.validacaoDasProximasPerguntas(mensagem, pessoa, erroTentativas, scanner);
 
-    validacao.calcularValidacaoFinal(erroTentativass);
+    validacao.calcularValidacaoFinal(pessoa, mensagem, erroTentativas);
 
-    // scanner.close();
-  }
+    scanner.close();
+  }
 }
