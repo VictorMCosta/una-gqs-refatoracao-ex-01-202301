9f3504236f352b772a9aa000ba16dc4f97c74c0f
Víctor Mendes Amaral Costa <victormendes0203@gmail.com>
2023-05-24 20:01:38 -0300

    fix: correção da variável NOME

--- Pessoa.java
+++ Pessoa.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Pessoa {
-  // public String nome;
+  public String nome;
   public int idade;
   public String cartaoVacinaEmDia;
   public String teveSintomasRecentemente;
@@ -10,15 +10,15 @@ public class Pessoa {
   public int porcentagemInfeccao;
   public String orientacaoFinal;
 
-  /*
-   * public void coletaNome(Pessoa pessoa, Scanner scanner) {
-   * System.out.println("\nInforme o seu nome: ");
-   * pessoa.nome = scanner.nextLine();
-   * }
-   */
+  
+    public void coletaNome(Pessoa pessoa, Scanner scanner) {
+    System.out.println("\nInforme o seu nome: ");
+    pessoa.nome = scanner.nextLine();
+    }
+   
 
   public void coletaIdade(Pessoa pessoa, Scanner scanner) {
     System.out.println("\nInforme a sua idade: ");
     pessoa.idade = scanner.nextInt();
-  }
+  }
 }
